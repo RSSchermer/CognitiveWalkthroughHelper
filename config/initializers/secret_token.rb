faaa3a897cf5927873b810f6ae049404c8198a8d
Roland S. Schermer <roland0507@gmail.com>
2014-02-18 21:27:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CognitiveWalkthroughHelper::Application.config.secret_key_base = '365a0bfb5127ae99a71ff4aafb74911b719a6c7887d8a91838715e562d5e44ed5c963f121f9908cacc51a8b6634c3a11aadbcbefb1c3764632201cb6c6cff806'
